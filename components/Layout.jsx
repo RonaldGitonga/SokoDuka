412ffd005bc9491b2f993d50f062e3b0846e7d6f
RonaldGitonga <rgatuiku@gmail.com>
2023-05-09 20:09:55 +0300

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+import  Navbar  from '../components/Navbar'
+import  Footer  from '../components/Footer'
+
+const Layout = ({children}) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Soko Duka</title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
